4df70e5934976f6e9619456d9f6478b38bf23cb9
Kazuya-san <kazuyasan3286@gmail.com>
2022-05-28 20:33:34 +0500

    added meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@
 // import Link from 'next/link'
 // import Layout from '../components/Layout'
 
-// const IndexPage = ({data}) => 
+// const IndexPage = ({data}) =>
 // {
 
 //   console.log(data)
-  
+
 //   return (
 //   <Layout title="Home | Next.js + TypeScript Example">
 //     <h1 className="text-3xl font-bold underline text-red-600">
 //       Hello world!
-//     </h1>    
+//     </h1>
 //     <p>
 //       <Link href="/about">
 //         <a>About</a>
@@ -20,7 +20,7 @@
 
 //     <div className="flex sm:flex-row flex-col items-center justify-center flex-wrap p-3" >
 //       {data.data.map(item => (
-       
+
 //         <div className="w-full sm:w-1/4 p-3 shadow flex flex-col items-center justify-center hover:bg-gray-700" key={item.mal_id}>
 //           <img src={item.images.jpg.image_url} alt={item.name} className="rounded w-5/5 h-4/4" />
 //           <Link href={item.url} target="_blank">
@@ -49,23 +49,34 @@
 // }
 
 // export default IndexPage shadow shadow-gray-600
-import {ReactElement} from "react"
+import { ReactElement } from "react";
+import Head from "next/head";
 
-const Home = () : ReactElement => {
+const Home = (): ReactElement => {
   return (
-    <div className="container flex items-center justify-left h-screen bg-gray-900 text-red-500">
-      <div className="border-1 rounded-lg  ml-3 p-[20px] w-screen flex  flex-col justify-center h-[300px]">
-        <h1 className="md:text-9xl text-4xl hover:text-cyan-400 cursor-pointer font-semibold">Kazuya Yuuki</h1>
-        {/* <p className="mt-3 font-thin none ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi porro nisi saepe nesciunt est rerum?
-        </p> */}
-      </div>
+    <>
+      <Head>
+        <title>Portfolio Home</title>
+        <meta name="description" content="Kazuya Yuuki" />
+        <meta property="og:title" content="Portfolio Home" />
+        <meta property="og:description" content="Kazuya Yuuki" />
+      </Head>
+      <div className="container flex items-center justify-left h-screen bg-gray-900 text-red-500">
+        <div className="border-1 rounded-lg  ml-3 p-[20px] w-screen flex  flex-col justify-center h-[300px]">
+          <h1 className="md:text-9xl text-4xl hover:text-cyan-400 cursor-pointer font-semibold">
+            Kazuya Yuuki
+          </h1>
+          {/* <p className="mt-3 font-thin none ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi porro nisi saepe nesciunt est rerum?
+          </p> */}
+        </div>
 
-      <div>
-        <img src="../pu" alt="" />
+        <div>
+          <img src="../pu" alt="" />
+        </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default Home;
